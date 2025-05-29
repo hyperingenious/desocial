089440942345f3dc454e3168473bf4ae84984e79
hyperingenious <skbmasale941@gmail.com>
2025-05-29 17:38:31 +0530

    clear: messagecontext cleared

--- src/contexts/MessageContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-// Create the AuthContext
-export const MessageContext = createContext();
-
-// Custom hook to use the AuthContext
-export function useMessageContext() {
-  return useContext(MessageContext);
-}
